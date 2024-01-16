dc3dba4a889265a89c12e733da69f2cc85bb9412
Destina Bartley (Personal) <83770604+DestinaBartley@users.noreply.github.com>
2024-01-16 09:18:31 +0000

    Add files via upload

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/javatraining/springbootcourse/rest/FunRestController.java
@@ -0,0 +1,19 @@
+package com.javatraining.springbootcourse.rest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/joke")
+    public String tellJoke() {
+        return "Knock knock...";
+    }
+
+}
---
+++ 01-spring-boot-overview/03-custom-application-properties/src/main/java/com/javatraining/springbootcourse/SpringbootcourseApplication.java
@@ -0,0 +1,15 @@
+package com.javatraining.springbootcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcourseApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootcourseApplication.class, args);
+
+	}
+
+}
